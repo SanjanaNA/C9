5e74d227d75e361a77b904354874911e3e12839d
SanjanaNA <86785200+SanjanaNA@users.noreply.github.com>
2021-07-01 09:28:00 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var box
+function setup() {
+  createCanvas(400,400);
+
+  box=createSprite(200,200,50,50)
+  box.shapeColor="blue";
+}
+
+function draw() 
+{
+  background("pink");
+
+
+
+  drawSprites()
+
+}
+
+
+
+
